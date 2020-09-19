5e588f8314bdf87d6a5ed36589d6debc94cc10bc
Devy V <sualveera@gmail.com>
2020-09-19 09:13:41 +0530

    Js bug fix

--- js/script.js
+++ js/script.js
@@ -120,50 +120,50 @@
       e.preventDefault();
     });
 
-    /*CountDown*/
-    $('.married_coundown').countdown({until: new Date("Feb 18, 2017 11:00:00")});
+    ///*CountDown*/
+//    $('.married_coundown').countdown({until: new Date("Feb 18, 2017 11:00:00")});
+//
+//    /*OWL Carousel in Our Story*/
+//    $(".story_wrapper").owlCarousel({
+//      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
+//      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 1], autoPlay: false,
+//      itemsMobile: [479, 1], itemsDesktopSmall: [980, 1], itemsDesktop: [1500, 2], mouseDrag: true
+//    });
 
-    /*OWL Carousel in Our Story*/
-    $(".story_wrapper").owlCarousel({
-      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
-      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 1], autoPlay: false,
-      itemsMobile: [479, 1], itemsDesktopSmall: [980, 1], itemsDesktop: [1500, 2], mouseDrag: true
-    });
-
-    /*Gallery Carousel */
-    $(".gallery_wrapper").owlCarousel({
-      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
-      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 2], autoPlay: true,
-      itemsMobile: [479, 1], mouseDrag: true
-    });
-
-    /*Registry Carousel */
-    $(".registry_wrapper").owlCarousel({
-      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
-      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 3, mouseDrag: true, autoPlay: true
-    });
-
-    /*The Crew Carousel*/
-    $(".guest_wrapper").owlCarousel({
-      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
-      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 4, mouseDrag: true, autoPlay: true
-    });
-
-    /*Slider Carousel*/
-    $(".slider").owlCarousel({
-      navigation: true,
-      responsive: true,
-      responsiveRefreshRate: 200,
-      slideSpeed: 200,
-      paginationSpeed: 200,
-      rewindSpeed: 500,
-      stopOnHover: false,
-      autoHeight: true,
-      singleItem: true,
-      mouseDrag: true,
-      autoPlay: true,
-      transitionStyle: "fade"
-    });
+   // /*Gallery Carousel */
+//    $(".gallery_wrapper").owlCarousel({
+//      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200,
+//      paginationSpeed: 200, rewindSpeed: 500, items: 3, itemsTablet: [768, 2], autoPlay: true,
+//      itemsMobile: [479, 1], mouseDrag: true
+//    });
+//
+//    /*Registry Carousel */
+//    $(".registry_wrapper").owlCarousel({
+//      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
+//      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 3, mouseDrag: true, autoPlay: true
+//    });
+//
+//    /*The Crew Carousel*/
+//    $(".guest_wrapper").owlCarousel({
+//      navigation: true, responsive: true, responsiveRefreshRate: 200, slideSpeed: 200, paginationSpeed: 200,
+//      rewindSpeed: 500, stopOnHover: true, autoHeight: true, items: 4, mouseDrag: true, autoPlay: true
+//    });
+//
+//    /*Slider Carousel*/
+//    $(".slider").owlCarousel({
+//      navigation: true,
+//      responsive: true,
+//      responsiveRefreshRate: 200,
+//      slideSpeed: 200,
+//      paginationSpeed: 200,
+//      rewindSpeed: 500,
+//      stopOnHover: false,
+//      autoHeight: true,
+//      singleItem: true,
+//      mouseDrag: true,
+//      autoPlay: true,
+//      transitionStyle: "fade"
+//    });
 
     /* Top Menu Click to Section */
     $('.sub_menu').find('a').on("click", function (e) {
